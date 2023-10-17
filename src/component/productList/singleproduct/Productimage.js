5d4df933a77e59ffb6116bfb8d21b44d0d2299e6
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2023-10-17 08:36:19 +0530

    img change

--- src/component/productList/singleproduct/Productimage.js
+++ src/component/productList/singleproduct/Productimage.js
@@ -2,11 +2,13 @@ import React from 'react'
 import img01 from '../../../images/productimages/img 01.jpg';
 import img02 from '../../../images/productimages/img 02.jpg';
 
+const imageArray = [img01,img02];
+
 export default function Productimage({images}) {
     const [imgSrc,setImgSrc] = React.useState(images[0]);
   return (
     <div style={{width:"100%",textAlign:"center"}}>
-      <img src={img01} 
+      <img src={imageArray[images[0]]}
       style={{width:"100%",height:"auto",borderRadius:"25px",objectFit:"contain",padding:"20px"}}
       onMouseOver={()=>setImgSrc(images[1])}
       onMouseOut={()=>setImgSrc(images[0])}
