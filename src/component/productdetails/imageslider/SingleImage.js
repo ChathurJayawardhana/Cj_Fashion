f60cb355d5b06f0d2d860e1c60b96c6ad09e8488
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2023-10-21 21:12:38 +0530

    get image zoom code

--- src/component/productdetails/imageslider/SingleImage.js
+++ src/component/productdetails/imageslider/SingleImage.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import ReactImageMagnify from 'react-image-magnify';
 
 export default function SingleImage() {
   return (
     <div>
-      chahura jayawardhana
+      <ReactImageMagnify {...{
+    smallImage: {
+        alt: 'Wristwatch by Ted Baker London',
+        isFluidWidth: true,
+        src: watchImg300
+    },
+    largeImage: {
+        src: watchImg1200,
+        width: 1200,
+        height: 1800
+    }
+}} />
     </div>
   )
 }
