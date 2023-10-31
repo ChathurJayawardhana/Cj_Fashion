52c3c7501b1c285795362c913983f6008e859aff
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2023-10-31 09:46:52 +0530

    Change qty

--- src/component/productdetails/productdetails2/qtyselect/Qtyselectsection.js
+++ src/component/productdetails/productdetails2/qtyselect/Qtyselectsection.js
@@ -15,19 +15,34 @@ const qtybuttons = [
 ]
 
 export default function Qtyselectsection() {
+
+const [qty,setQty] = React.useState(1);
+
+const handleqtyChange = (type) =>{
+   if("add" === type && qty+1 < 11) {
+       setQty(qty+1);
+   }else if("remove" === type && qty-1>0){
+    setQty(qty-1);
+   }
+}
+
   return (
-    <div>
+    
         <Grid container>
            <Grid item>
            <Typography
               style={{fontSize:"30px",borderBottom:"1px solid black",float:"left"}}
-             >01</Typography>
+             >
+              {qty < 10 ? "0" + qty : qty}
+             </Typography>
            </Grid>
            {qtybuttons.map((val,key)=>{
            return(
            <Grid item xs={1} md={2} key={key} style={{marginLeft:"15px"}}>
             
-            <IconButton style={{
+            <IconButton 
+            onClick={()=>handleqtyChange(val.type)}
+            style={{
                 border:"1px solid grey",
                 borderRadius:"10px",
                 height:"45px",
@@ -41,6 +56,6 @@ export default function Qtyselectsection() {
                )
             })}
         </Grid>
-    </div>
+    
   )
 }
