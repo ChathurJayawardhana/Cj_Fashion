f4eeb0e9852c152d5f9fbaee3cd9bfbfb8ea7786
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2023-11-12 23:20:45 +0530

    update

--- src/layout/allitems/AllItemLayout.js
+++ src/layout/allitems/AllItemLayout.js
@@ -4,6 +4,37 @@ import { Grid } from '@mui/material'
 import ProductList from './productlist/ProductList'
 
 export default function AllItemLayout() {
+  const [productlist,setProductlist] = React.useState([]);
+/*
+  {
+    productName : "product 0",
+    productType :"dress5",
+    price:"8400",
+    size:["xs","sm","L"],
+    images:[0,2],
+    description:"This is the description of above product",
+    availbleqty:1000,
+    sku : "SKU001",
+  },*/
+
+  React.useEffect(()=>{
+    let tempProductlist = [];
+
+
+    for(let i=0;i<100;i++){
+      
+      let randType1 = Math.round(Math.random()*100)%5;
+      let randType2 = Math.round(Math.random()*100)%5;
+      let randType3 = Math.round(Math.random()*100)%5;
+
+      let tempProduct = {
+         productName : `Product ${i+1}`,
+         description:`This is the description ${i+1} of above product`,
+         price : (Math.floor(Math.randam()*(100-4))+5)*100,
+      };
+      console.log(tempProduct);
+    }
+  },[])
   return (
     <Commonlayout>
       <Grid container>
